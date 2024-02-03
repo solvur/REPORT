95b11f852c541ba7df171b32694ffe434478b42b
solvur <125721159+solvur@users.noreply.github.com>
2024-02-03 09:22:23 +0300

    pen.py

--- pen.py
+++ pen.py
@@ -2,7 +2,7 @@ print('Ваше фамилия имя')
 name = input()
 print('Какой ваш любимый предмет в школе')
 less = input()
-print('ФИОвашего классного руководителя')
+print('ФИО вашего классного руководителя')
 name2 = input()
 
 print(f'{name}\n{less}\n{name2}')
