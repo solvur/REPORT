ce6d8377b5be19207b87865e268ee561113156b6
solvur <125721159+solvur@users.noreply.github.com>
2024-02-03 09:21:48 +0300

    REPORT
    Prepared by 
    Cheked by Gulmira Karimova

---
+++ pen.py
@@ -0,0 +1,8 @@
+print('Ваше фамилия имя')
+name = input()
+print('Какой ваш любимый предмет в школе')
+less = input()
+print('ФИОвашего классного руководителя')
+name2 = input()
+
+print(f'{name}\n{less}\n{name2}')
